1c12dd93a3d458e1bede9fd8adc25ed00ef9e09d
marcosrib <marcos.rib.sousa@gmail.com>
2022-03-08 18:17:44 -0300

    feat: Adicionado geração de fila e topico automático

---
+++ config/localstack/sqs-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+apt-get install -y jq > /dev/null 2>&1
+
+sns_topic_arn=`awslocal sns create-topic --name topic-teste | jq -r '.TopicArn'`
+
+sqs_queue_names=('teste_queue')
+
+for i in "${!sqs_queue_names[@]}"; do
+ sns_subscriptions[i]=`awslocal sns subscribe --topic-arn $sns_topic_arn --protocol sqs \
+   --notification-endpoint $(awslocal sqs create-queue --queue-name ${sqs_queue_names[i]} | jq -r '.QueueUrl')`;
+done
+
+awslocal sqs list-queues
+awslocal sns list-subscriptions
